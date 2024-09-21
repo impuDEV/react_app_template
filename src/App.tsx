9d9fe662c9f6c87483e400b8dceab9e2956ca39a
Papernov Alexander <impudev@gmail.com>
2024-09-21 16:08:14 +0300

    [styling] provided style theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Suspense } from 'react';
-import './index.scss';
+import './styles/index.scss';
 import {Link, Route, Routes} from "react-router-dom";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>THEME</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
